51c81cc9e415f64233e3d144c2d75d1ba59de5c0
HaichaoLihc <lhc20050616@berkeley.edu>
2024-03-04 23:26:11 -0800

    Initial Commit

---
+++ Genre selector.js
@@ -0,0 +1,91 @@
+document.getElementById('all').addEventListener('click', ()=>{
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        if (allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    }
+})
+
+// Genre = light 
+document.getElementById('B1').addEventListener('click', () => {
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        
+        if (allImage[i].classList.contains('light')===false){
+            allImage[i].classList.add('hide');
+        }
+        if (allImage[i].classList.contains('light')&&allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    } 
+})
+
+// Genre = water 
+document.getElementById('B2').addEventListener('click', () => {
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        if (allImage[i].classList.contains('water')===false){
+            allImage[i].classList.add('hide');
+        }
+        if (allImage[i].classList.contains('water')&&allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    } 
+})
+
+// Genre = landscape
+document.getElementById('B3').addEventListener('click', () => {
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        if (allImage[i].classList.contains('landscape')===false){
+            allImage[i].classList.add('hide');
+        }
+        if (allImage[i].classList.contains('landscape')&&allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    } 
+})
+
+// Genre = street
+document.getElementById('B4').addEventListener('click', () => {
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        if (allImage[i].classList.contains('street')===false){
+            allImage[i].classList.add('hide');
+        }
+        if (allImage[i].classList.contains('street')&&allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    } 
+})
+
+// Genre = people
+document.getElementById('B5').addEventListener('click', () => {
+    let allImage = document.querySelectorAll('div.photos>div');
+    for(i=0;i<allImage.length;i++){
+        if (allImage[i].classList.contains('people')===false){
+            allImage[i].classList.add('hide');
+        }
+        if (allImage[i].classList.contains('people')&&allImage[i].classList.contains('hide')){
+            allImage[i].classList.remove('hide');
+        }
+    } 
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.genreSelector button');
+
+    buttons.forEach(link => {
+        link.addEventListener('click', function () {
+
+            // Remove the 'active' class from all links
+            buttons.forEach(otherLink => {
+                otherLink.classList.remove('active');
+            });
+
+            // Add the 'active' class to the clicked link
+            this.classList.add('active');
+        });
+    });
+});
